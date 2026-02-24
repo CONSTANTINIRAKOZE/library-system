77b107d385052adb89c6a5240bcaba28f8727979
Gasasira Constantin <irakozconstantin@gmail.com>
2026-02-24 10:44:31 +0200

    changed permissions

--- library_system/utils.py
+++ library_system/utils.py
@@ -9,12 +9,14 @@ def track_access(func):
     return wrapper
 
 # Access Control Closure: Checks if the user has the required role (e.g., "Admin") [cite: 290, 291]
+# 2. Access Control Closure
 def permission_check(required_role):
     def decorator(func):
-        def wrapper(user_role, *args, **kwargs):
+        # We add 'self' here so it properly catches the object, leaving 'user_role' to catch the string!
+        def wrapper(self, user_role, *args, **kwargs):
             if user_role != required_role:
                 print(f"Access Denied! You need '{required_role}' privileges to do this.")
                 return None
-            return func(*args, **kwargs)
+            return func(self, user_role, *args, **kwargs)
         return wrapper
     return decorator
